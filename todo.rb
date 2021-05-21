1b9fdf3da4ce0daf387365b8cabb0bcd498bc824
Joshua Morgan <jmorg0605@gmail.com>
2021-05-21 12:50:40 -0400

    added routes for lists

--- todo.rb
+++ todo.rb
@@ -2,6 +2,34 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do 
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+ redirect "/lists"
+end
+
+# View list of lists
+get "/lists" do
+  @lists = session[:lists]
+  erb(:lists, layout: :layout)
+end
+
+# Render the new list form
+get "/lists/new" do 
+  erb(:new_list, layout: :layout)
+end
+
+# Create a new list
+post "/lists" do 
+  p params
+  session[:lists] << {name: params[:list_name], todos: []}
+  p session[:lists]
+  redirect "/lists"
 end
