cae1f6d4ef63c9abc98439c029f1917fd5e410f1
Joshua Morgan <jmorg0605@gmail.com>
2021-05-22 12:16:50 -0400

    created todo page and route

--- todo.rb
+++ todo.rb
@@ -21,23 +21,49 @@ get "/lists" do
   erb(:lists, layout: :layout)
 end
 
+
 # Render the new list form
 get "/lists/new" do 
   erb(:new_list, layout: :layout)
 end
 
+get "/lists/:id" do 
+  # get the todos from this particular list
+  # send to view template
+ @number = params[:id].to_i
+ @list = session[:lists][@number]
+ 
+ erb(:list, layout: :layout)
+end
+
+post "/list/:id" do 
+  @number = params[:id].to_i
+ todo = params[:todo_item]
+ @list = session[:lists][@number]
+  erb(:todos, layout: :layout)
+end
+
+# Return an error message if the name is invalid. Return nil if valid.
+def error_for_list_name(name)
+  if !(1..100).cover?(name.size)
+    return  "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? {|list| list[:name] == name}
+    return  "List name must be unique."
+  end
+end 
+
 # Create a new list
 post "/lists" do 
   # remove unnecessary white-space
   list_name = params[:list_name].strip
-  # Have user enter list name again if nothing is entered or is more than 100 characters
-  if (1..100).cover?(list_name.size)
+  
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb(:new_list, layout: :layout)
+  else
     session[:lists] << {name: list_name, todos: []}
     session[:success] = "The list has been created."
     redirect "/lists"
-  else
-    session[:error] = "List name must be between 1 and 100 characters."
-    erb(:new_list, layout: :layout)
-  end
-  
+  end 
 end
