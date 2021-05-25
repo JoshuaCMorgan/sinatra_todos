52b743065d2fed099a838c6dadd2550ac28944e6
Joshua Morgan <jmorg0605@gmail.com>
2021-05-25 09:46:31 -0400

    add todos to a list

--- todo.rb
+++ todo.rb
@@ -32,8 +32,8 @@ end
 get "/lists/:id" do 
   # get the todos from this particular list
   # send to view template
- @id = params[:id].to_i
- @list = session[:lists][@id]
+ @list_id = params[:id].to_i
+ @list = session[:lists][@list_id]
  
  erb(:list, layout: :layout)
 end
@@ -47,6 +47,38 @@ get "/lists/:id/edit" do
   erb(:edit_list, layout: :layout)
 end
 
+# Return an error message if the name is invalid. Return nil if valid.
+helpers do 
+  def error_for_list_name(name)
+    if !(1..100).cover?(name.size)
+      return  "List name must be between 1 and 100 characters."
+    elsif session[:lists].any? {|list| list[:name] == name}
+      return  "List name must be unique."
+    end
+  end 
+
+  def error_for_todo(name)
+    if !(1..100).cover?(name.size)
+      "Todo must be between 1 and 100 characters."
+    end
+  end
+end 
+
+# Create a new list
+post "/lists" do 
+  # remove unnecessary white-space
+  list_name = params[:list_name].strip
+  
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb(:new_list, layout: :layout)
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end 
+end
 
 # Udpate an exisiting todo list
 post "/lists/:id" do
@@ -66,33 +98,6 @@ post "/lists/:id" do
   end
 end
 
-# Return an error message if the name is invalid. Return nil if valid.
-helpers do 
-  def error_for_list_name(name)
-    if !(1..100).cover?(name.size)
-      return  "List name must be between 1 and 100 characters."
-    elsif session[:lists].any? {|list| list[:name] == name}
-      return  "List name must be unique."
-    end
-  end 
-end 
-
-# Create a new list
-post "/lists" do 
-  # remove unnecessary white-space
-  list_name = params[:list_name].strip
-  
-  error = error_for_list_name(list_name)
-  if error
-    session[:error] = error
-    erb(:new_list, layout: :layout)
-  else
-    session[:lists] << {name: list_name, todos: []}
-    session[:success] = "The list has been created."
-    redirect "/lists"
-  end 
-end
-
 # Delete a list from session
 post "/lists/:id/delete" do 
   id = params[:id].to_i
@@ -105,11 +110,17 @@ end
 
 # Add a new todo to a list
 post "/lists/:list_id/todos" do 
-  list_id = params[:list_id].to_i
-  list = session[:lists][list_id]
-  list[:todos] << {name: params[:todo], completed: false}
-  
-  session[:success] = "The todo was added."
-  
-  redirect "lists/#{list_id}"
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb(:list, layout: :layout)
+  else
+    @list[:todos] << {name: text, completed: false}
+    session[:success] = "The todo was added."
+    redirect "lists/#{@list_id}"
+  end
 end
