0548b83b2889988a8ad8c3b151390db210ea655f
Joshua Morgan <jmorg0605@gmail.com>
2021-06-05 20:15:51 -0400

    made XHR calls

--- todo.rb
+++ todo.rb
@@ -136,8 +136,12 @@ end
 post "/lists/:id/delete" do 
   id = params[:id].to_i
   session[:lists].delete_at(id)
-  session[:success] = "The list has been deleted."
-  redirect "/lists"
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    "/lists"
+  else
+    session[:success] = "The list has been deleted."
+    redirect "/lists"
+  end
 end
 
 def error_for_todo(name)
@@ -170,8 +174,13 @@ post "/lists/:list_id/todos/:id/delete" do
   
   todo_id = params[:id].to_i
   @list[:todos].delete_at(todo_id)
-  session[:success] = "The todo has been deleted."
-  redirect "/lists/#{@list_id}"
+
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    status 204
+  else
+    session[:success] = "The todo has been deleted."
+    redirect "/lists/#{@list_id}"
+  end
 end
 
 # Update the status of a todo: complete or undo complete
