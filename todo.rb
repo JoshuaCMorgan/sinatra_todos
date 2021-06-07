51e740acbc2198d394407007f80003bb7534e8f3
Joshua Morgan <jmorg0605@gmail.com>
2021-06-06 21:47:47 -0400

    changed todo id association from index to id number

--- todo.rb
+++ todo.rb
@@ -85,8 +85,13 @@ helpers do
   def sort_todos(todos, &block)
     complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed] }
 
-    incomplete_todos.each { |todo| yield(todo, todos.index(todo)) }
-    complete_todos.each  { |todo| yield(todo, todos.index(todo)) }
+    incomplete_todos.each(&block)
+    complete_todos.each(&block)
+  end
+
+  def next_todo_id(todos)
+    max = todos.map { |todo| todo[:id] }.max || 0
+    max + 1
   end
 end 
 
@@ -155,13 +160,16 @@ post "/lists/:list_id/todos" do
   @list_id = params[:list_id].to_i
   @list = load_list(@list_id)
   text = params[:todo].strip
-  
+
   error = error_for_todo(text)
   if error
     session[:error] = error
     erb(:list, layout: :layout)
   else
-    @list[:todos] << {name: text, completed: false}
+    # give each new todo a unique identifier, name, completed state.
+    id = next_todo_id(@list[:todos])
+    @list[:todos] << {id: id, name: text, completed: false}
+    p @list
     session[:success] = "The todo was added."
     redirect "/lists/#{@list_id}"
   end
@@ -173,7 +181,8 @@ post "/lists/:list_id/todos/:id/delete" do
   @list = load_list(@list_id)
   
   todo_id = params[:id].to_i
-  @list[:todos].delete_at(todo_id)
+  @list[:todos].reject! { |todo| todo[:id] == todo_id }
+  p @list
 
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     status 204
@@ -190,7 +199,8 @@ post "/lists/:list_id/todos/:id" do
   
   todo_id = params[:id].to_i
   is_completed = params[:completed] == "true"
-  @list[:todos][todo_id][:completed] = is_completed
+  todo = @list[:todos].find {|todo| todo[:id] == todo_id }
+  todo[:completed] = is_completed
 
   session[:success] = "The todo has been updated."
   redirect "/lists/#{@list_id}"
